51d23a1c91bae4b5a78aabfdbf6c0d8b6a6af42a
ivanvrdev <ivangaston20032017@gmail.com>
2022-05-26 14:06:37 -0300

    Modelo de n al cuadrado entrenado 26-05-2022

--- n_al_cuadrado/scripts.js
+++ n_al_cuadrado/scripts.js
@@ -1,46 +1,136 @@
-// datos
 
-let dataX = []
-let dataY = []
+const trainModel = async () =>{
+    // datos
+    let dataX = []
+    let dataY = []
 
-const dataSize = 10
-const stepSize = 0.001
+    const dataSize = 10
+    const stepSize = 0.001
 
-for (let index = 0; index < dataSize; index+=stepSize) {
-    dataX.push(index)
-    dataY.push(index * index)
+    for (let index = 0; index < dataSize; index+=stepSize) {
+        dataX.push(index)
+        dataY.push(index * index)
+    }
+
+    //Modelo
+
+    const tensorX = tf.tensor(dataX)
+    const tensorY = tf.tensor(dataY)
+
+    const model = tf.sequential()
+
+    // model.add(tf.layers.dense({inputShape: [1], units: 1}))
+    model.add(tf.layers.dense({inputShape: [1], units: 20, activation: 'relu'}))
+    model.add(tf.layers.dense({inputShape: [1], units: 20, activation: 'relu'}))
+    model.add(tf.layers.dense({units: 1}))
+
+    model.compile({
+        loss: "meanSquaredError",
+        optimizer: "adam",
+        metrics: ['accuracy']
+    })
+
+    const surface = {name: 'Visor', tab: 'Entrenamiento'}
+
+    await model.fit(tensorX, tensorY, {
+        epochs: 30,
+        batchSize: 64,
+        callbacks: tfvis.show.fitCallbacks(surface, ['loss', 'acc'])
+    })
+
+    return model
 }
 
-//Modelo
+const loadModel = async (path) => {
+    const model = await tf.loadLayersModel(path)
+    return model
+}
 
-const tensorX = tf.tensor(dataX)
-const tensorY = tf.tensor(dataY)
+const saveModel = () =>{
+    model.save('downloads://my-model').then(()=>console.log("Modelo guardado"))
+}
 
-const model = tf.sequential()
+const predictSquare = (num) =>{
+    model.predict(tf.tensor([num])).print()
+}
 
-// model.add(tf.layers.dense({inputShape: [1], units: 1}))
-model.add(tf.layers.dense({inputShape: [1], units: 20, activation: 'relu'}))
-model.add(tf.layers.dense({units: 1}))
+let model = undefined
 
-model.compile({
-    loss: "meanSquaredError",
-    optimizer: "adam",
-    metrics: ['accuracy']
+//html tags
+const trainButton = document.getElementById("train")
+const saveButton = document.getElementById("save")
+const loadButton = document.getElementById("load")
+const predictButton = document.getElementById("predict")
+
+saveButton.style.display = "none"
+predictButton.style.display = "none"
+
+//events
+trainButton.addEventListener("click", async () =>{
+    loadButton.style.display = "none"
+    trainButton.style.display = "none"
+    model = await trainModel()
+    saveButton.style.display = "block"
+    predictButton.style.display = "block"
 })
 
-const surface = {name: 'Visor', tab: 'Entrenamiento'}
+saveButton.addEventListener("click", ()=>{
+    saveModel()
+})
 
-model.fit(tensorX, tensorY, {
-    epochs: 30,
-    batchSize: 64,
-    callbacks: tfvis.show.fitCallbacks(surface, ['loss', 'acc'])
+loadButton.addEventListener("click", async ()=>{
+    trainButton.style.display = "none"
+    loadButton.style.display = "none"
+    model = await loadModel('model/my-model.json')
+    predictButton.style.display = "block"
 })
-.then(log => {
-    // tfvis.show.history(surface, log, ['loss', 'acc'])
-    // tfvis.visor().open()
-    
-    model.predict(tf.tensor([7])).print()
+
+predictButton.addEventListener("click", ()=>{
+    predictSquare(7)
 })
+
+// datos
+// let dataX = []
+// let dataY = []
+
+// const dataSize = 10
+// const stepSize = 0.001
+
+// for (let index = 0; index < dataSize; index+=stepSize) {
+//     dataX.push(index)
+//     dataY.push(index * index)
+// }
+
+// //Modelo
+
+// const tensorX = tf.tensor(dataX)
+// const tensorY = tf.tensor(dataY)
+
+// const model = tf.sequential()
+
+// // model.add(tf.layers.dense({inputShape: [1], units: 1}))
+// model.add(tf.layers.dense({inputShape: [1], units: 20, activation: 'relu'}))
+// model.add(tf.layers.dense({units: 1}))
+
+// model.compile({
+//     loss: "meanSquaredError",
+//     optimizer: "adam",
+//     metrics: ['accuracy']
+// })
+
+// const surface = {name: 'Visor', tab: 'Entrenamiento'}
+
+// model.fit(tensorX, tensorY, {
+//     epochs: 30,
+//     batchSize: 64,
+//     callbacks: tfvis.show.fitCallbacks(surface, ['loss', 'acc'])
+// })
+// .then(log => {
+//     // tfvis.show.history(surface, log, ['loss', 'acc'])
+//     // tfvis.visor().open()
+//     model.save('downloads://my-model').then(()=>console.log("Modelo guardado en descargas"))
+//     model.predict(tf.tensor([7])).print()
+// })
 
 
 
